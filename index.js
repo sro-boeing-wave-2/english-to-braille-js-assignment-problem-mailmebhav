6015c895741204e7eb6021335102960c49aa70a1
Bhavin Patel <mailmebhav@gmail.com>
2018-07-10 10:54:34 +0530

    Modified index.js to convert english text to braille

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+import englishToBrailleLiteralSet from './english-to-braille.js';
 /**
- * Converts text in english to braille code.
- * Write all code related to the DOM Manipulation here.
- * Using the english-to-braille mapper convert the text
- * in english language to that in the equivalent braille
- * code.
- */
+* Converts text in english to braille code.
+* Write all code related to the DOM Manipulation here.
+* Using the english-to-braille mapper convert the text
+* in english language to that in the equivalent braille
+* code.
+*/
+function respond() {
+  document.getElementById('targetLangText').innerHTML = '';
+  const mes = document.getElementById('sourceLangText').value;
+  const myMap = new Map(englishToBrailleLiteralSet);
+  let i;
+  let brailletxt = '';
+  for (i = 0; i < mes.length; i += 1) {
+    brailletxt += myMap.get(mes[i]);
+  }
+  document.getElementById('targetLangText').innerHTML = brailletxt;
+}
+function initfunction() {
+  document.getElementById('btnConvertEnglishToBraille').onclick = respond;
+}
+window.onload = initfunction;
